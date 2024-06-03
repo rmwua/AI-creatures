910ed1af27aa143b3f73378d575dd77939362e96
Polina Volskaia <meloniel@yandex.ru>
2024-06-03 12:38:52 +0300

    added 2 functions to geonme class with unittests

--- genome.py
+++ genome.py
@@ -1,3 +1,13 @@
+import numpy as np
+
 class Genome:
-    def __init__(self):
-        pass
+    @staticmethod
+    def get_random_gene(length):
+        gene = np.array([np.random.random() for _ in range(length)])
+        return gene
+
+    @staticmethod
+    def get_random_genome(gene_length, gene_count):
+        genome = [Genome.get_random_gene(gene_length) for _ in range(gene_count)]
+        return genome
+
--- test_genome.py
+++ test_genome.py
@@ -8,7 +8,27 @@ class GenomeTest(unittest.TestCase):
         self.assertIsNotNone(genome.Genome)
 
     def test_random_gene(self):
-        self.assertIsNotNone(genome.get_random_gene())
+        self.assertIsNotNone(genome.Genome.get_random_gene(5))
+
+    def test_randon_gene_not_none(self):
+        self.assertIsNotNone(genome.Genome.get_random_gene(5))
+
+    def test_randon_gene_has_values(self):
+        gene = genome.Genome.get_random_gene(5)
+        self.assertIsNotNone(gene[0])
+
+    def test_randon_gene_length(self):
+        gene = genome.Genome.get_random_gene(20)
+        self.assertEqual(len(gene), 20)
+
+    def test_randon_gene_is_numpy_array(self):
+        gene = genome.Genome.get_random_gene(20)
+        self.assertEqual(type(gene), np.ndarray)
+
+    def test_randon_genome_exists(self):
+        data = genome.Genome.get_random_genome(20, 5)
+        self.assertIsNotNone(data)
+
 
 
 unittest.main()
